6553bb2a2c3d92ad07e6b41dbc288a37728f42f4
Jerico Victoria <78258443+dormammuuuuu@users.noreply.github.com>
2023-04-18 00:37:21 +0800

    Sidebar Update and TaskViewPage Fixed

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import { useRouter, useSearchParams } from 'next/navigation';
-
-const TaskViewPage = () => {
-  const router = useRouter();
-  const id = useSearchParams();
-
-  console.log('router', router);
-    console.log('id', id);
-  return <div>TaskViewPage {id}</div>;
+const TaskViewPage = ({ params }: {
+    params: Array<string>;
+}) => {
+  return <div>TaskViewPage {params.id}</div>;
 };
 
 export default TaskViewPage;
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { db } from '../config';
 import Link from 'next/link'
 
 const Sidebar = (): JSX.Element => {
-  const [lists, setLists] = useState<{ id: number; name: string }[]>([]);
+  const [lists, setLists] = useState<{ id: string; name: string }[]>([]);
   const [showNew, setShowNew] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const [input, setInput] = useState<string>('');
@@ -30,15 +30,21 @@ const Sidebar = (): JSX.Element => {
     setShowNew(true);
   };
 
+  const firebaseAdd = (name: string) => {
+    // sanitize input turn it to slug
+    const slug_id = name.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+    try {
+      db.ref("lists/" + slug_id).set({name: input});
+    } catch (error) {
+      console.error('Error adding:', error);
+    }
+    setLists([...lists, { id: slug_id, name: input }]);
+  };
+
   const handleClickOutside = (event: MouseEvent) => {
     if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
       if (input !== '') {
-        try {
-          db.ref("lists/" + input).set({color: 'blue-500'});
-        } catch (error) {
-          console.error('Error adding document:', error);
-        }
-        setLists([...lists, { id: lists.length + 1, name: input }]);
+        firebaseAdd(input);
       }
       setShowNew(false);
       setInput('');
@@ -48,12 +54,7 @@ const Sidebar = (): JSX.Element => {
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       if (input !== '') {
-        try {
-          db.ref("lists/" + input).set({color: 'blue-500'});
-        } catch (error) {
-          console.error('Error adding document:', error);
-        }
-        setLists([...lists, { id: lists.length + 1, name: input }]);
+        firebaseAdd(input);
       }
       setShowNew(false);
       setInput('');
@@ -64,15 +65,19 @@ const Sidebar = (): JSX.Element => {
     setInput(e.currentTarget.value);
   };
 
+  const firstLetter = (str: string) => {
+    return str.charAt(0).toUpperCase();
+  };
+
   return (
     <div className="relative h-full p-3">
-      <div>
-				<h1 className="text-2xl font-bold">To-Do Lists</h1>
+      <div className='m-3'>
+				<h1 className="text-2xl font-bold">To-Do List</h1>
       </div>
-      <div>
-        <ul>
+      <div className='my-7'>
+        <ul className="space-y-1">
           {lists.map((list) => (
-            <li className=" my-2" key={list.id}><Link href={`/${list.id}`}>{list.id}</Link></li>
+            <li className="flex items-center gap-3 my-2 hover:bg-white/10 p-2 rounded-sm" key={list.id}><div className='bg-blue-500 rounded-sm w-6 flex justify-center items-center text-xs py-1'>{ firstLetter(list.id) }</div><Link href={`/${list.id}`}>{list.name}</Link></li>
           ))}
           {showNew && (
             <input type="text" className="w-full bg-transparent border-b-2" onInput={handleInput} onKeyDown={handleKeyPress} ref={inputRef} />
